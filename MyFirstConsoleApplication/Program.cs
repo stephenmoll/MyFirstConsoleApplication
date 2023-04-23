c63df51153ddadd652b2fe03dc39fa0e8bc627ad
stephenmoll <stephenmollandco@mail.com>
2023-04-24 00:18:12 +0200

    adding text labels

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -26,12 +26,22 @@ class GlazerCalc
 
         double width, height, woodLength, glassArea;
         string widthString, heightString;
+
+        Console.Write("Enter the width in feet: ");
         widthString = Console.ReadLine();
         width = double.Parse(widthString);
-        heightString = Console.ReadLine();      // No clue what to do here
+
+        Console.Write("Enter the hight in feet: ");
+        heightString = Console.ReadLine();      
         height = double.Parse(heightString);
+
         woodLength = 2 * (width + height) * 3.25;
         glassArea = 2 * (width * height);
+
+        Console.WriteLine("The length of the wood is " +
+        woodLength + " feet");
+        Console.WriteLine("The area of the glass is " +
+        glassArea + " square metres");
 
         Console.ReadKey();
     }
